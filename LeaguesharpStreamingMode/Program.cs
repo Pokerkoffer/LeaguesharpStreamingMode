e9e18f5d3efcf36b2d4b80b774a4c690a5a18583
stefsot <stefsot@windowslive.com>
2015-01-22 13:12:46 +0200

    Update Program.cs

--- LeaguesharpStreamingMode/Program.cs
+++ LeaguesharpStreamingMode/Program.cs
@@ -64,7 +64,7 @@ namespace LeaguesharpStreamingMode
             offsets.Add("4.19", new Int32[] { 0x5F40, 0x9B60, 0x9B40 });
             offsets.Add("4.20", new Int32[] { 0x6040, 0x9C00, 0x9BE0 });
             offsets.Add("4.21", new Int32[] { 0x6420, 0xA320, 0xA1B5 });
-            offsets.Add("5.1", new Int32[] { 0x6350, 0xA1A0, 0x0 });
+            offsets.Add("5.1", new Int32[] { 0x6440, 0xA290, 0x0 });
         }
 
         static void Enable()
